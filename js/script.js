b55facbd15cb0b8bd5c8180b588be63961ff49a7
Maciej Ptaszynski <mpsendit@gmail.com>
2022-02-27 23:03:52 +0100

    add 5.4

--- js/script.js
+++ js/script.js
@@ -29,13 +29,65 @@ function titleClickHandler(event){
   const targetArticle = document.querySelector(articleSelector)
   console.log(targetArticle)
 
-  /* [IN PROGRESS] add class 'active' to the correct article */
+  /* [DONE] add class 'active' to the correct article */
 
   targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
-
+console.log(links)
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
+// [IN PROCESS] module 5.4
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+
+  const titleList = document.querySelectorAll(optTitleListSelector);
+  titleList.innerHTML = '';
+  console.log(titleList);
+
+  /* for each article */
+
+  const articles = document.querySelectorAll(optArticleSelector);
+    let html = '';
+    for(let article of articles){
+
+      /* get the article id */
+
+      const articleId = article.getAttribute('id');
+      console.log('articleId', articleId);
+
+      /* find the title element */
+    
+
+      /* get the title from the title element */
+
+      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+      console.log(articleTitle);
+
+      /* create HTML of the link */
+      const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+      console.log('linkHTML', linkHTML);
+
+      /* insert link into html variable */
+
+      html = html + linkHTML;
+  }
+    /* insert link into titleList */
+    titleList.innerHTML = html;
+    const links = document.querySelector('.titles a');
+    console.log(links);
+    for(let link of links){
+      link.addEventListener('click', titleClickHandler);
+    }
+}
+
+generateTitleLinks();
